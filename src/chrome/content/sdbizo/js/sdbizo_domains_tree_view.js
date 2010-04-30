fad990e54e6a3eabab0ac22f57865fac22726d22
Donnie Flood <donald.flood@alumni.duke.edu>
2010-04-29 19:06:49 -0700

    update license copyright

--- src/chrome/content/sdbizo/js/sdbizo_domains_tree_view.js
+++ src/chrome/content/sdbizo/js/sdbizo_domains_tree_view.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Bizo, Inc (Donnie Flood [donnie@bizo.com])
+ * Copyright 2010 Bizo, Inc (Donnie Flood [donnie@bizo.com])
  *  
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
  * file except in compliance with the License. You may obtain a copy of the License at 
